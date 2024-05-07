I fix bug in data normalization.

src/scnn/private/interface/data.py
@@ -27,7 +27,9 @@ def normalized_into_input_space(
         A :math:`(\\ldots \\times d)` tensor of weights in the original
         input space.
     """
-    return weights / column_norms
+    return np.divide(
+        weights, column_norms, out=np.zeros_like(weights), where=column_norms != 0
+    )
 
 
 def input_into_normalized_space(model_weights, column_norms):

src/scnn/private/utils/data/transforms.py
@@ -63,7 +63,11 @@ def unitize_columns(
     train_set = (X_train, train_set[1])
 
     if test_set is not None:
-        test_set = (test_set[0] / column_norms, test_set[1])
+        X_test, y_test = test_set
+        np.divide(
+            X_test, column_norms, out=np.zeros_like(X_test), where=column_norms != 0
+        )
+        test_set = (X_test, y_test)
 
     return train_set, test_set, column_norms
 